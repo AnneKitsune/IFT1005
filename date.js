154d4882bb2a7665277c1e2f8ebd724bd079189a
Joël Lupien (Jojolepro) <jojolepromain@gmail.com>
2018-12-05 17:15:09 -0500

    Fixed leading 0 date

--- date.js
+++ date.js
@@ -1,10 +1,12 @@
 // day/month/year hrs:min:sec
 
+//https://stackoverflow.com/questions/3605214/javascript-add-leading-zeroes-to-date
+
 var today = new Date();
-document.getElementById("day").innerHTML = today.getDate();
-document.getElementById("month").innerHTML = today.getMonth();
+document.getElementById("day").innerHTML = ('0' + today.getDate()).slice(-2);
+document.getElementById("month").innerHTML = ('0' + today.getMonth()).slice(-2);
 document.getElementById("year").innerHTML = today.getFullYear();
 
-document.getElementById("hours").innerHTML = today.getHours();
-document.getElementById("minutes").innerHTML = today.getMinutes();
-document.getElementById("seconds").innerHTML = today.getSeconds();
+document.getElementById("hours").innerHTML = ('0' + today.getHours()).slice(-2);
+document.getElementById("minutes").innerHTML = ('0' + today.getMinutes()).slice(-2);
+document.getElementById("seconds").innerHTML = ('0' + today.getSeconds()).slice(-2);
